3d0885d3d5cd6a25dd949aaa43a7c4f141221753
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-09-22 10:39:40 -0300

    style: Initial margin and padding adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-64">sidebar</aside>
-        <main className="flex-1">main</main>
+        <aside className="w-72 bg-zinc-950 p-6">sidebar</aside>
+        <main className="flex-1 p-6">main</main>
       </div>
-      <footer>footer</footer>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">footer</footer>
     </div>
   );
 }
