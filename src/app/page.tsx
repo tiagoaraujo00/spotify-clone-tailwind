e75d2b810990afb28ae75124d6134d591234783e
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-09-22 10:23:38 -0300

    style: Defining the structure of the initial blocks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-
 export default function Home() {
   return (
-    <div className="h-screen">
-      <div>
-        <aside>sidebar</aside>
-        <main>main</main>
+    <div className="h-screen flex flex-col">
+      <div className="flex flex-1">
+        <aside className="w-64">sidebar</aside>
+        <main className="flex-1">main</main>
       </div>
       <footer>footer</footer>
     </div>
-  )
+  );
 }
