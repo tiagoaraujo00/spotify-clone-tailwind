38dcbdd580ebb01bb8ccc2d1687f0accef091ec2
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-09-22 10:50:59 -0300

    style: Adding icons to the sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
+import { Home as HomeIcon, Search, Library } from "lucide-react";
+
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
-        <aside className="w-72 bg-zinc-950 p-6">sidebar</aside>
+        <aside className="w-72 bg-zinc-950 p-6">
+          <nav>
+            <a href="">
+              {" "}
+              <HomeIcon />
+              Home
+            </a>
+            <a href="">
+              {" "}
+              <Search /> Search
+            </a>
+            <a href="">
+              <Library />
+              Your library
+            </a>
+          </nav>
+        </aside>
         <main className="flex-1 p-6">main</main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">footer</footer>
+      <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
+        footer
+      </footer>
     </div>
   );
 }
