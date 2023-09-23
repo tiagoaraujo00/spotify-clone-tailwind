845446c53d51ac978eb9b01544428c01b3392a98
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-09-23 16:56:19 -0300

    feat: playlists finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
-import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight } from "lucide-react";
+import {
+  Home as HomeIcon,
+  Search,
+  Library,
+  ChevronLeft,
+  ChevronRight,
+  Play,
+} from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -36,10 +43,18 @@ export default function Home() {
             </a>
           </nav>
           <nav className="mt-10 pt-10 border-t border-zinc-800 flex flex-col gap-3">
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Hot hits Brasil</a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Aniver Funk</a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">My Playlist</a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Top Brasil</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Hot hits Brasil
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Aniver Funk
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              My Playlist
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Top Brasil
+            </a>
           </nav>
         </aside>
         <main className="flex-1 p-6">
@@ -52,25 +67,67 @@ export default function Home() {
             </button>
           </div>
           <h1 className="font-semibold text-text-3xl mt-10">Good Afternoon</h1>
-          <div className="grid grid-cols-3 gap-4">
-            <div className="bg-white/10 rounded-">
-              <Image src="/album.jpg" width={80} height={80} alt="ANTI"/>
-            </div>
-            <div className="bg-white/10 rounded-">
-              <Image src="/album.jpg" width={80} height={80} alt="ANTI"/>
-            </div>
-            <div className="bg-white/10 rounded-">
-              <Image src="/album.jpg" width={80} height={80} alt="ANTI"/>
-            </div>
-            <div className="bg-white/10 rounded-">
-              <Image src="/album.jpg" width={80} height={80} alt="ANTI"/>
-            </div>
-            <div className="bg-white/10 rounded-">
-              <Image src="/album.jpg" width={80} height={80} alt="ANTI"/>
-            </div>
-            <div className="bg-white/10 rounded-">
-              <Image src="/album.jpg" width={80} height={80} alt="ANTI"/>
-            </div>
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <a
+              href="#"
+              className="bg-white/5 group flex items-center gap-4 rounded overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image src="/album.jpg" width={104} height={104} alt="ANTI" />
+              <strong>ANTI</strong>
+              <button className="w-12 h-12 invisible group-hover:visible flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group flex items-center gap-4 rounded overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image src="/album.jpg" width={104} height={104} alt="ANTI" />
+              <strong>ANTI</strong>
+              <button className="w-12 h-12 invisible group-hover:visible flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group flex items-center gap-4 rounded overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image src="/album.jpg" width={104} height={104} alt="ANTI" />
+              <strong>ANTI</strong>
+              <button className="w-12 h-12 invisible group-hover:visible flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group flex items-center gap-4 rounded overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image src="/album.jpg" width={104} height={104} alt="ANTI" />
+              <strong>ANTI</strong>
+              <button className="w-12 h-12 invisible group-hover:visible flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group flex items-center gap-4 rounded overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image src="/album.jpg" width={104} height={104} alt="ANTI" />
+              <strong>ANTI</strong>
+              <button className="w-12 h-12 invisible group-hover:visible flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8">
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group flex items-center gap-4 rounded overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image src="/album.jpg" width={104} height={104} alt="ANTI" />
+              <strong>ANTI</strong>
+              <button className="w-12 h-12 invisible group-hover:visible flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8">
+                <Play />
+              </button>
+            </a>
           </div>
         </main>
       </div>
