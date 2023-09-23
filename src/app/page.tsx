fa2a46b3e54e14588d22532f8fd9527e0aceab12
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-09-23 09:45:06 -0300

    style: finished color spheres

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,12 @@ export default function Home() {
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
         <aside className="w-72 bg-zinc-950 p-6">
-          <nav className="space-y-5">
+          <div className="flex items-center gap-2">
+            <div className="w-3 h-3 rounded-full bg-red-700"></div>
+            <div className="w-3 h-3 rounded-full bg-yellow-500"></div>
+            <div className="w-3 h-3 rounded-full bg-green-500"></div>
+          </div>
+          <nav className="space-y-5 mt-10">
             <a
               href=""
               className="flex items-center gap-2 text-sm font-semibold text-zinc-200"
