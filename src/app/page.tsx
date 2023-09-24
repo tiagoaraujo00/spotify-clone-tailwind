6676a83d0c021fe15a14245c66c969d38b1ffc8e
tiagoaraujo00 <tiagodevjs@gmail.com>
2023-09-24 17:09:37 -0300

    feat: music timer finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ import {
   ChevronLeft,
   ChevronRight,
   Play,
+  Shuffle,
+  SkipBack,
+  SkipForward,
+  Repeat,
 } from "lucide-react";
 import Image from "next/image";
 
@@ -167,8 +171,23 @@ export default function Home() {
             <span className="text-xs text-zinc-400">Rihanna</span>
           </div>
         </div>
-        <div className="flex flex-col items-center">
-          <div></div>
+        <div className="flex flex-col items-center gap-2">
+          <div className="flex items-center gap-6">
+            <Shuffle  size={20} className="text-zinc-200"/>
+            <SkipBack  size={20} className="text-zinc-200"/>
+            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black">
+                <Play />
+              </button>
+              <SkipForward  size={20} className="text-zinc-200"/>
+              <Repeat  size={20} className="text-zinc-200"/>
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-xs text-zinc-400">0:31</span>
+            <div className="h-1 rounded-full w-96 bg-zinc-600">
+              <div className="bg-zinc-200 w-40 h-1 rounded-full"></div>
+            </div>
+            <span className="text-xs text-zinc-400">2:14</span>
+          </div>
         </div>
         <div>
 
